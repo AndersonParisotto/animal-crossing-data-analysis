1ab749e24ef675496f24698175f6b411a5792d12
Anderson Parisotto <alparisotto@ucs.br>
2026-01-19 08:44:26 -0300

    estrutura inicial e carregamento automatico dos dados

---
+++ import.py
@@ -0,0 +1,18 @@
+import pandas as pd
+#leitura de múltiplos arquivos CSV em um único DataFrame
+import glob
+
+#definindo o caminho onde os arquivos CSV estão localizados
+path = './dados'
+all_files = glob.glob(path + "/*.csv")
+
+#criando um dicionário para armazenar os DataFrames
+dataframe = {}
+for filename in all_files:
+    #extraindo o nome do arquivo sem a extensão para usar como chave no dicionário
+    name - filename.split('/')[-1].split('.')[0]
+    #lendo o arquivo CSV e armazenando no dicionário
+    dataframe[name] = pd.read_csv(filenbame, index_col=None, header=0)
+
+#imprimindo as chaves do dicionário para verificar os DataFrames carregados
+print(dataframe.keys())
